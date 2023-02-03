590957f01a19f2d6edce70fb01b8f59990333539
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-02-04 00:09:43 +0530

    Import pictures

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,4 +1,12 @@
 import logo from '../assets/mahesh.png';
 import backgroundPic from '../assets/bgLinks.png';
 
-export default {logo, backgroundPic};
+import circle from '../assets/circle.png';
+import java from '../assets/java.png';
+import react from '../assets/react.png';
+import sass from '../assets/sass.png';
+import spring_boot from '../assets/react.png';
+import android from '../assets/androidstudio.png';
+import mypic from '../assets/mypic.jpg';
+
+export default {logo, backgroundPic, circle, java, react, sass, spring_boot,android, mypic};
