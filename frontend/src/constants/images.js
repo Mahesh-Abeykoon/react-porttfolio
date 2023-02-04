23dbb9a33437869a159f34d94cd369ef231fd5a0
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-02-04 17:57:38 +0530

    Import new iconsr

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -5,8 +5,12 @@ import circle from '../assets/circle.png';
 import java from '../assets/java.png';
 import react from '../assets/react.png';
 import sass from '../assets/sass.png';
-import spring_boot from '../assets/react.png';
+import spring_boot from '../assets/springboot.png';
 import android from '../assets/androidstudio.png';
 import mypic from '../assets/mypic.jpg';
 
-export default {logo, backgroundPic, circle, java, react, sass, spring_boot,android, mypic};
+import firebase from '../assets/firebase.png';
+import git from '../assets/git.png';
+
+
+export default {logo, backgroundPic, circle, java, react, sass, spring_boot,android, mypic, firebase, git};
