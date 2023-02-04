4130f8230fd09bbbcc48ca17a1ee4451ea051b7c
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-02-04 17:56:38 +0530

    Update Header

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -39,15 +39,17 @@ const Header = () => {
     </motion.div>
 
     <motion.div
-      whileInView={{ opacity: [0, 1]}}
+      whileInView={{ opacity: [0, 1] }}
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
     >
       <img src={images.mypic} alt="profile_bg" />
+      
       <motion.img
-        whileInView={{ opacity: [0,1 ]}}
-        transition={{ duration: 1, ease: 'easeInOut' }}
         src={images.circle}
+        whileInView={{ scale: [0,1] }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+      
         alt="profile_circle"
         className="overlay_circle"
       />
@@ -58,9 +60,9 @@ const Header = () => {
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
     >
-      {[images.java, images.react, images.spring_boot, images.android, images.sass].map((circle, index) => (
+      {[images.java, images.spring_boot, images.android, images.react, images.sass, images.firebase, images.git].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
-          <img scr={circle} alt="profile_bg" />
+          <img src={circle} alt="profile_bg" />
         </div>
       ))}
     </motion.div> 
