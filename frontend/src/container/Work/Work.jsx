d04e42d8b8ab0713ace9049a7dc935eecb13af80
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-02-10 22:57:59 +0530

    Implement portfolio section

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+
 import './Work';
 
 const Work = () => {
+
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
+  const handleWorkFilter = (item) => {
+
+  }
+
   return (
-    <div>Work</div>
+    <>
+      <h2 className="head-text">My Portfolio </h2>
+      <div className="app__work-filter">
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
