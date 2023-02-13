d587da7786ef8656cf00c67777b02781bb1a8a74
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-02-13 23:24:35 +0530

    Import schema

--- sanity-project/schemas/index.js
+++ sanity-project/schemas/index.js
@@ -1,5 +1,7 @@
 // schemas/index.js
 import about from './about'
 import works from './works'
+import skills from './skills'
+import experiences from './experiences'
 
-export const schemaTypes = [about, works]
+export const schemaTypes = [about, works, skills, experiences]
